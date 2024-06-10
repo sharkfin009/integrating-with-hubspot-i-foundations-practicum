0ad5d1ea82584275b1f221f77eb993ed019be963
Ben Amato <ben.amato@struto.co.uk>
2024-06-10 14:47:55 +0200

    First commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,12 +9,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_TOKEN = process.env.PRIVATE_APP_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const spaceships = 'https://api.hubspot.com/crm/v3/objects/spaceships';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    }   
+    const params = {
+        properties:"name,length,width,has_light_speed_drive"
+        
 
+
+    }
+    try {
+        const resp = await axios.get(spaceships, { headers, params });
+        const data = resp.data.results;
+        console.log(data);
+        res.render('spaceships', { title: 'Custom Objects | HubSpot APIs', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
