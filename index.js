44b0d2fe7b3b37905d62f1bede35b8082a1b1e46
Ben Amato <ben.amato@struto.co.uk>
2024-06-10 16:09:48 +0200

    create homepage and update routes

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_TOKEN = process.env.PRIVATE_APP_TOKEN;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+// homepage route
 app.get('/', async (req, res) => {
     const spaceships = 'https://api.hubspot.com/crm/v3/objects/spaceships';
     const headers = {
@@ -22,71 +20,62 @@ app.get('/', async (req, res) => {
     }   
     const params = {
         properties:"name,length,width,has_light_speed_drive"
-        
-
-
     }
     try {
         const resp = await axios.get(spaceships, { headers, params });
         const data = resp.data.results;
         console.log(data);
-        res.render('spaceships', { title: 'Custom Objects | HubSpot APIs', data });
+        res.render('spaceships', { title: 'Spaceships', data });
     } catch (error) {
         console.error(error);
     }
 });
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/update-cobj', async (req, res) => {
+    const spaceships = 'https://api.hubspot.com/crm/v3/objects/spaceships';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
         'Content-Type': 'application/json'
+    }   
+    const params = {
+        properties:"name,length,width,has_light_speed_drive"
     }
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(spaceships, { headers, params });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        console.log(data);
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });
     } catch (error) {
         console.error(error);
     }
-});
 
-* * App.post sample
+})
+
 app.post('/update', async (req, res) => {
     const update = {
         properties: {
-            "favorite_book": req.body.newVal
+            "name" : req.body.name,
+            "length": req.body.length,
+            "width": req.body.width,
+            "has_light_speed_drive": req.body.has_light_speed_drive
         }
     }
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+    const name = req.query.name;
+    const updateSpaceships = `https://api.hubapi.com/crm/v3/objects/spaceships/${name}?idProperty=name`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
     try { 
-        await axios.patch(updateContact, update, { headers } );
+        await axios.patch(updateSpaceships, update, { headers } );
         res.redirect('back');
     } catch(err) {
         console.error(err);
     }
 
 });
-*/
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
