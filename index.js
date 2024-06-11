a36c0bf22c21efaf05c954bdecc36cee69bcf036
Ben Amato <ben.amato@struto.co.uk>
2024-06-11 15:07:47 +0200

    add view, add, update view, update, delete all working

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const PRIVATE_APP_TOKEN = process.env.PRIVATE_APP_TOKEN;
 
 
-// homepage route
+// homepage view route
 app.get('/', async (req, res) => {
     const spaceships = 'https://api.hubspot.com/crm/v3/objects/spaceships';
     const headers = {
@@ -19,20 +19,20 @@ app.get('/', async (req, res) => {
         'Content-Type': 'application/json'
     }   
     const params = {
-        properties:"name,length,width,has_light_speed_drive"
+        properties:"name,length,width,has_light_speed_drive,hs_object_id"
     }
     try {
         const resp = await axios.get(spaceships, { headers, params });
         const data = resp.data.results;
-        console.log(data);
-        res.render('spaceships', { title: 'Spaceships', data });
+        res.render('homepage', { title: 'Spaceships', data });
     } catch (error) {
         console.error(error);
     }
 });
 
-app.get('/update-cobj', async (req, res) => {
-    const spaceships = 'https://api.hubspot.com/crm/v3/objects/spaceships';
+// 'add' view route
+app.get('/add-cobj', async (req, res) => {
+    let getUrl = 'https://api.hubspot.com/crm/v3/objects/spaceships';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
         'Content-Type': 'application/json'
@@ -41,41 +41,109 @@ app.get('/update-cobj', async (req, res) => {
         properties:"name,length,width,has_light_speed_drive"
     }
     try {
-        const resp = await axios.get(spaceships, { headers, params });
+        const resp = await axios.get(getUrl, { headers, params });
         const data = resp.data.results;
-        console.log(data);
-        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });
+        res.render('add', { title: 'Spaceships', data });
     } catch (error) {
         console.error(error);
     }
-
 })
 
-app.post('/update', async (req, res) => {
-    const update = {
+//'add' route
+app.post('/add-cobj', async (req, res) => {
+    const spaceship = {
         properties: {
             "name" : req.body.name,
             "length": req.body.length,
             "width": req.body.width,
-            "has_light_speed_drive": req.body.has_light_speed_drive
+            "has_light_speed_drive": req.body.has_light_speed_drive=="on" ? "true" : "false"
         }
     }
 
-    const name = req.query.name;
-    const updateSpaceships = `https://api.hubapi.com/crm/v3/objects/spaceships/${name}?idProperty=name`;
+    const createUrl = `https://api.hubapi.com/crm/v3/objects/spaceships`;
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
         'Content-Type': 'application/json'
     };
 
     try { 
-        await axios.patch(updateSpaceships, update, { headers } );
-        res.redirect('back');
+        await axios.post(createUrl, spaceship, { headers } );
+        res.redirect('http://localhost:3000');
     } catch(err) {
         console.error(err);
     }
 
 });
+
+// get 'update' view route
+app.get('/update-cobj/:name', async (req, res) => {
+    const params = {
+        properties:"name,length,width,has_light_speed_drive"
+    }
+    const getUrl = `https://api.hubspot.com/crm/v3/objects/spaceships/${req.params.name}?idProperty=name`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(getUrl, { headers,params });
+        let data = resp.data.properties;
+        
+        res.render('update', { title: 'Update Spaceship | Integrating With HubSpot I Practicum',data });
+    } catch (error) {
+        // console.error(error);
+    }
+})
+
+// 'update'  route
+app.post('/update-cobj/:name', async (req, res) => {
+    const update = {
+        properties: {
+            hs_object_id: req.body.hs_object_id,
+            name: req.body.name,
+            length: req.body.length,
+            width: req.body.width,
+            has_light_speed_drive: req.body.has_light_speed_drive=="on" ? "true" : "false"
+        }
+    }
+    
+    const updateUrl = `https://api.hubapi.com/crm/v3/objects/spaceships/${req.params.name}?idProperty=name`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.patch(updateUrl, update, { headers } );
+        res.redirect('http://localhost:3000');
+    } catch(err) {
+        console.error(err);
+    }
+
+});
+
+// delete route
+
+app.get('/delete-cobj/:id', async (req, res) => {
+    
+    const  delUrl= `https://api.hubapi.com/crm/v3/objects/spaceships/${req.params.id}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.delete(delUrl,  { headers } );
+        res.redirect('http://localhost:3000');
+    } catch(err) {
+        console.error(err);
+    }
+
+});
+
+
+
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
